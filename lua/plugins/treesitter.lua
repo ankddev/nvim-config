377b28a589a8ffb5f1fba39b365bb0f4028e3183
Andrey <andrekabatareika@gmail.com>
2026-03-26 10:44:54 +0300

    chore: remove useless comment

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     branch = "master",
-    version = false, -- last release is way too old and doesn't work on Windows
+    version = false,
     build = ":TSUpdate",
     event = "VeryLazy",
     lazy = vim.fn.argc(-1) == 0,
