a39d7099240b74dd6b8dafdd2173e4975bd7ed88
Andrey <ankddev@duck.com>
2026-04-03 16:41:39 +0300

    style: reformat file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -118,13 +118,17 @@ return {
       },
     },
     keys = {
-      { "<leader>x",  desc = "Trouble" },
-      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",              desc = "Diagnostics (Trouble)" },
+      { "<leader>x", desc = "Trouble" },
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
       { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
-      { "<leader>xs", "<cmd>Trouble symbols toggle<cr>",                  desc = "Symbols (Trouble)" },
-      { "<leader>xS", "<cmd>Trouble lsp toggle<cr>",                      desc = "LSP references/definitions/... (Trouble)" },
-      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>",                  desc = "Location List (Trouble)" },
-      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>",                   desc = "Quickfix List (Trouble)" },
+      { "<leader>xs", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols (Trouble)" },
+      {
+        "<leader>xS",
+        "<cmd>Trouble lsp toggle<cr>",
+        desc = "LSP references/definitions/... (Trouble)",
+      },
+      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
       {
         "[q",
         function()
@@ -173,7 +177,7 @@ return {
           {
             "rafamadriz/friendly-snippets",
             config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
+              require("luasnip.loaders.from_vscode").lazy_load()
             end,
           },
         },
